dce29a21d8198be57c64fa97469cb0ee74614fed
Jasper den Ouden <o.jasper@gmail.com>
2015-09-11 22:07:50 +0200

    Get rid of some superfluous files

--- storebin/lib/encode_list_plain.lua
+++
@@ -1,8 +0,0 @@
-local encode
-
-local function encode_list_plain(write, list)
-   encode = encode or require "storebin.lib.encode"
-   for _, el in ipairs(list) do encode(write, el) end
-end
-
-return encode_list_plain
