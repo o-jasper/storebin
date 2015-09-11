4b4c2d9a5843af79bb29e886bae15d46fd825d3f
Jasper den Ouden <o.jasper@gmail.com>
2015-09-11 23:16:50 +0200

    More towards profiling..(though with unsuitable test set) Found issue with seed 1442011300

--- storebin/test/test.lua
+++ storebin/test/test.lua
@@ -1,8 +1,13 @@
 local assert_eq, gen_tree = unpack(require "storebin.test.lib")
 
-local encode = require "storebin.plain_encode"
 local decode = require "storebin.decode"
-local opt_encode = require "storebin.compress_encode"
+local plain_encode = require "storebin.plain_encode"
+local compress_encode = require "storebin.compress_encode"
+
+local storer = {
+   { encode=plain_encode,    decode=decode, name="plain" },
+   { encode=compress_encode, decode=decode, name="compress" },
+}
 
 local seed = tonumber(arg[1])
 if not seed then
@@ -10,22 +15,31 @@ if not seed then
    while x < 1000 do x = x * 10 end  -- Crude but whatevs/
    seed = os.time() + math.floor(x)
 end
-print("Seed:", seed)
+print("# Seed:", seed)
 math.randomseed(seed)
 
-for i = 1, tonumber(arg[2]) or 20 do
-   local t, n = gen_tree(true,6, {mini=10000, maxi=10002})
-   print("Run", i, n)
-   
-   local d1 = encode(t)
-   local t1 = decode(d1)
-   assert_eq(t1, t, nil, " @plain")
-   assert_eq(t, t1, nil, " @plain(rev)")
+local function clock()
+   return math.floor(os.clock()*1e9 + 0.5)
+end
 
-   local d2 = opt_encode(t)
-   local t2 = decode(d2)
-   assert_eq(t2, t, nil, " @opt")
-   assert_eq(t, t2, nil, " @opt(rev)")
---   -- This being essentially noise, not much chance of winning.
-   if #d1 ~= #d2 then print("", #d1, #d2) end
+print("#  For each one between brackets")
+print("# N [plainencode_time decode_time length]")
+
+for i = 1, tonumber(arg[2]) or 20 do
+   local tab, n = gen_tree(true,6, {mini=10000, maxi=10002})
+   
+   local ret = {n}
+   for _, el in ipairs(storer) do
+      local pt = clock()
+      local data = el.encode(tab)
+      local dt = clock()
+      local a_tab = el.decode(data)
+      local ft = clock()
+      table.insert(ret, dt - dt)
+      table.insert(ret, ft - dt)
+      table.insert(ret, #data)
+      assert_eq(a_tab, tab,   nil, " @" .. el.name .. "")
+      assert_eq(tab,   a_tab, nil, " @" .. el.name .. "(rev)")
+   end
+   print(unpack(ret))
 end
