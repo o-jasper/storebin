b7504059719b1131fbaddfe8edade9106565599a
Jasper den Ouden <o.jasper@gmail.com>
2015-09-23 23:10:20 +0200

    broke that earlier. Also beware of the other return values...

--- storebin/pkg/file_encode_via_string.lua
+++ storebin/pkg/file_encode_via_string.lua
@@ -1,8 +1,12 @@
 return function(encode)
    return function(file, data, may_be_nil) 
-      assert( may_be_nil or data ~= nil )  -- Otherwise confusionly returns nil.
-      fd:write(encode(data))
-      fd:close()
-      return true
+      local fd = io.open(file, "w")
+      if fd then
+         assert( may_be_nil or data ~= nil )  -- Otherwise confusionly returns nil.
+         local str = encode(data)
+         fd:write(str)
+         fd:close()
+         return true
+      end
    end
 end
