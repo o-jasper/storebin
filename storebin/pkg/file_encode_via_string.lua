ee1850674b8db6df9e4e0182a7f5655f171c9ac1
Jasper den Ouden <o.jasper@gmail.com>
2015-09-23 23:03:31 +0200

    Actually go via string

--- storebin/pkg/file_encode_via_string.lua
+++ storebin/pkg/file_encode_via_string.lua
@@ -1,12 +1,8 @@
 return function(encode)
    return function(file, data, may_be_nil) 
       assert( may_be_nil or data ~= nil )  -- Otherwise confusionly returns nil.
-      local fd = io.open(file, "w")
-      if fd then
-         local str = encode(data)
-         fd:write(str)
-         fd:close()
-         return true
-      end
+      fd:write(encode(data))
+      fd:close()
+      return true
    end
 end
