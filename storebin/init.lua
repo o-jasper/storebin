e7ed3ea578fdeb585b57ecdcc1340c24f38a0953
Jasper den Ouden <o.jasper@gmail.com>
2016-05-19 03:28:03 +0200

    Forgot commas and `decode`, dumb..

--- storebin/init.lua
+++ storebin/init.lua
@@ -1,10 +1,11 @@
 return {  -- Note have policy of explicit `require`s now..
    __constant = true,
    encode = require "storebin.encode",
-   plain_encode = require "storebin.plain_encode"
-   compress_encode = require "storebin.compress_encode"
-   file_encode = require "storebin.file_encode"
-   file_plain_encode = require "storebin.file_plain_encode"
-   file_compress_encode = require "storebin.file_compress_encode"
-   file_decode = require "storebin.file_decode"
+   decode = require "storebin.decode",
+   plain_encode = require "storebin.plain_encode",
+   compress_encode = require "storebin.compress_encode",
+   file_encode = require "storebin.file_encode",
+   file_plain_encode = require "storebin.file_plain_encode",
+   file_compress_encode = require "storebin.file_compress_encode",
+   file_decode = require "storebin.file_decode",
 }
