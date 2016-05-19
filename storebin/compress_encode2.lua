dd22e5406fd8eb74cd57602540c8c97b3c4fe435
Jasper den Ouden <o.jasper@gmail.com>
2016-05-19 19:29:48 +0200

    The problem was simple typo; `done[i]` instead of `done[k]`

--- storebin/compress_encode2.lua
+++ storebin/compress_encode2.lua
@@ -15,24 +15,35 @@ end
 
 local encode = string_ize(require "storebin.lib.encode")
 local encode_uint = string_ize(require "storebin.lib.encode_uint")
-local encode_bool_arr = string_ize(require "storebin.lib.encode_bool_arr")
+--local encode_bool_arr = string_ize(require "storebin.lib.encode_bool_arr")
 
 local function incr(got, data)
-   if true or #data == 1 then return data end
+   assert(type(data) == "string")
+   if #data == 1 then return data end
+
    local el = got.els[data] or { cnt = 0 }
    el.cnt = el.cnt + 1
    got.els[data] = el
    return data
 end
 
-local function do_leaves(tab, got)  -- Only do the leaves
-   local ret = {}  -- Note: encodes everything, just assembling with that from here.
-   for k,v in pairs(tab) do
-      local data_k = incr(got, encode(k))
-      if type(v) == "table" then
-         ret[data_k] = do_leaves(v, got)
+-- Encodes all the leaves.
+local function do_leaves(tab, got)
+   local function handle_1(x)
+      if type(x) == "table" then
+         return do_leaves(x, got)
       else
-         ret[data_k] = incr(got, encode(v))
+         return incr(got, encode(x))
+      end
+   end
+   local ret, done = {}, {}
+   for i, el in ipairs(tab) do
+      done[i] = true
+      ret[i] = handle_1(el)
+   end
+   for k,v in pairs(tab) do
+      if not done[k] then
+         ret[incr(got, encode(k))] = handle_1(v)
       end
    end
    return ret
@@ -44,17 +55,19 @@ end
 
 -- Encode one.
 local function e_1(data, got)
-   if #data == 1 then return data end
+   assert(type(data) == "string")
+   local sz = #data
+   if sz == 1 then return data end
 
    local el = got.els[data]
-   assert(el, data)
    if el.n then  -- Already has index, write out saying the definition is used.
       return encode_use_def(el.n)
       -- If number of elements great enough to compress, figure an integer.
-   elseif el.cnt*#data > #data + 1 + math.ceil(math.log(16*got.n+1, 128)) then
-      el.n = got.n
+   elseif el.cnt*sz > 1 + sz*math.ceil(math.log(16*got.n+17, 128)) then
       got.n = got.n + 1  -- Increment the current integer.
+      el.n = got.n
       table.insert(got.list, data)
+      assert(el.n == #got.list)
       return encode_use_def(el.n)
    else  -- Cheaper to just put it here.
       el.cnt = el.cnt - 1  -- This one no longer option for shortening.
@@ -68,10 +81,10 @@ local function kv_list(tab)
       done[i] = true
       table.insert(list, el)
    end
-   for kd,v in pairs(tab) do
-      assert(type(k) == "string")
+   for k,v in pairs(tab) do
       if not done[k] then
-         table.insert(kv_list, {kd,v})
+         assert(type(k) == "string")
+         table.insert(kv_list, {k,v})
       end
    end
    table.sort(kv_list, function(a,b) return a[1] > b[1] end)
@@ -80,11 +93,11 @@ end
 
 -- Continue, work together the prepped things, use the definition if needed.
 local function _compress_encode(tab, got)
-   local olist, kv_list = kv_list(do_leaves(tab, got))
-   local list = {}  -- TODO identify all-boolean stuff.
-   for _,el in ipairs(olist) do  -- First pass, put together any sub-tables.
+   local list, kv_list = kv_list(tab)
+   -- TODO identify all-boolean stuff.
+   for i,el in ipairs(list) do  -- First pass, put together any sub-tables.
       if type(el) == "table" then
-         table.insert(list, incr(got, _compress_encode(el, got)))
+         list[i] = incr(got, _compress_encode(el, got))
       end
    end
    for _,el in ipairs(kv_list) do
@@ -112,9 +125,9 @@ local function compress_encode(tab, got)
       local got = got or {els={}, n=0, list={}}
       assert(type(got) == "table" and type(got.els) == "table"
                 and type(got.n) == "number" and got.n%1 == 0)
-      local str = _compress_encode(tab, got)
+      local pass = do_leaves(tab, got)
+      local str = _compress_encode(pass, got)
 
-      assert(got.n == 0)
       assert(#got.list == got.n)
       return encode_uint(got.n) .. table.concat(got.list) .. str
    else -- Zero is the empty definition list.
